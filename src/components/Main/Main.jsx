0a4730b0ebf428f20b71e05248ac98025d99ed65
Maximo Pavesi <machipavesi@gmail.com>
2023-03-02 12:08:13 -0300

    arreglos

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,13 +2,11 @@ import React from "react";
 import { ItemDetail } from "../Item/ItemDetail";
 import {ItemListContainer} from "../Item/ItemListContainer";
 import { Routes, Route } from "react-router-dom";
-import {Landing} from "../landing/Landing";
 
 export const Main = () => {
     return (
         <main>
             <Routes>
-                <Route path='/' element= {<Landing/>}/>
                 <Route path='/producto/:producto.Id' element= {<ItemDetail/>}/>
                 <Route path='/productos' element= {<ItemListContainer/>}/>
                 <Route path='/productos/:categoria/:subcategoria' element= {<ItemListContainer/>}/>
